3bf7fcb2ab7f9b27589e2918b8731f175179844a
Adam Bien <abien@adam-bien.com>
2024-11-05 12:09:18 +0100

    String.contentEquals() over String.equals() 👉 https://youtube.com/shorts/u9W3DgeL57E

---
+++ S127_ContentEqualsOverEquals.java
@@ -0,0 +1,8 @@
+interface S127_ContentEqualsOverEquals {
+
+  static void main(String... args) {
+    var first = "duke";
+    var second = new StringBuilder("duke");
+    System.out.println(first.contentEquals(second));
+  }
+}
