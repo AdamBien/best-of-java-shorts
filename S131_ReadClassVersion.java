c0c98dc808e3384809aecf47416765a6f5ac2fc7
Adam Bien <abien@adam-bien.com>
2024-11-15 12:19:56 +0100

    Read class version from byte code / .class 👉 https://youtube.com/shorts/5NDKkonTgG8?feature=share

--- S131_ReadClassVersion.java
+++ S131_ReadClassVersion.java
@@ -2,10 +2,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-interface App {
+interface S131_ReadClassVersion {
 
   static void main(String... args) throws IOException {
-    var classPath = Path.of("App.class");
+    var classPath = Path.of("S131_ReadClassVersion.class");
     var version = Files.readAllBytes(classPath)[7];
     System.out.println(version);
 
