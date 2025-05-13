d57d69b842cf76c597bb239aa81d54a3a5760e1d
Adam Bien <abien@adam-bien.com>
2025-05-13 21:05:22 +0200

    superfluous variable removed

--- S0045_Peek.java
+++ S0045_Peek.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 public class S0045_Peek {
   public static void main(String[] args) {
-    var result = List.of("a", "b", "c")
+     List.of("a", "b", "c")
         .stream()
         .filter(s -> s.startsWith("b"))
         .peek(System.out::println)
