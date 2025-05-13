8b828e8af95f21be2fe9d5b9f5d7c6788d3b318e
Adam Bien <abien@adam-bien.com>
2025-05-13 21:51:05 +0200

    duplicates removed

--- S0100_DetectOS.java
+++
@@ -1,7 +0,0 @@
-interface App {
-
-  static void main(String... args) {
-    var os =  System.getProperty("os.name");
-    System.out.println(os);
-  }
-}
--- S0100_InstantParse.java
+++
@@ -1,9 +0,0 @@
-import java.time.Instant;
-
-interface App {
-
-  static void main(String... args) {
-    var instant = Instant.parse("2024-12-12T10:00:00Z");
-    System.out.println(instant);
-  }
-}
